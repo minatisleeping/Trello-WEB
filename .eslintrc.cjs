0e6adbdd43e4b53784c77fc17e73bcdf07e0f642
minatisleeping <minatt2002@gmail.com>
2023-11-26 11:43:53 +0700

    add import eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,12 +16,22 @@ module.exports = {
     'react-refresh'
   ],
   rules: {
+    // React
     'react-refresh/only-export-components': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
 
+    // MUI
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
+      }
+    ],
+
+    //Common
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
