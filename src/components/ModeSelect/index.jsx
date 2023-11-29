f4bf615ba9174da15a088f470e092d32a27a3562
minatisleeping <minatt2002@gmail.com>
2023-11-29 16:58:49 +0700

    build app bar p3

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -17,7 +17,7 @@ function ModeSelect() {
   }
 
   return (
-    <FormControl size="small">
+    <FormControl size="small" sx={{ minWidth: 120 }}>
       <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
       <Select
         labelId="label-select-dark-light-mode"
