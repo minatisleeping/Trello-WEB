e317ceac84ce48916e975a69c5dd11a0e3ab98e2
minatisleeping <minatt2002@gmail.com>
2023-12-06 14:05:48 +0700

    build trello board content part2

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -0,0 +1,49 @@
+import { Card as MuiCard } from '@mui/material'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import GroupIcon from '@mui/icons-material/Group'
+import ForumIcon from '@mui/icons-material/Forum'
+import AttachmentIcon from '@mui/icons-material/Attachment'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
+function Card({ temporaryHideMedia }) {
+  if (temporaryHideMedia) {
+    return (
+      <MuiCard sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+        overflow: 'unset'
+      }}>
+        <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+          <Typography>Card test 01</Typography>
+        </CardContent>
+      </MuiCard>
+    )
+  }
+  return (
+    <MuiCard sx={{
+      cursor: 'pointer',
+      boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+      overflow: 'unset'
+    }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="https://drive.google.com/uc?id=1Now0X3Y_2xfz68zv4-7pV12IF3ohpYLM"
+      />
+      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+        <Typography>Minatisleeping MERN Stack</Typography>
+      </CardContent>
+      <CardActions sx={{ p: '0 4px 8px 4px' }}>
+        <Button size="small" startIcon={<GroupIcon />}>20</Button>
+        <Button size="small" startIcon={<ForumIcon />}>15</Button>
+        <Button size="small" startIcon={<AttachmentIcon />}>10</Button>
+      </CardActions>
+    </MuiCard>
+  )
+}
+
+export default Card
--- src/theme.js
+++ src/theme.js
@@ -3,13 +3,17 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 const APP_BAR_HEIGHT = '58px'
 const BOARD_BAR_HEIGHT = '60px'
 const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+const COLUMN_HEADER_HEIGHT = '50px'
+const COLUMN_FOOTER_HEIGHT = '56px'
 
 // Create a theme instance
 const theme = extendTheme({
   trello: {
     appBarHeight: APP_BAR_HEIGHT,
     boardBarHeight: BOARD_BAR_HEIGHT,
-    boardContentHeight: BOARD_CONTENT_HEIGHT
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
+    columnHeaderHeight: COLUMN_HEADER_HEIGHT,
+    columnFooterHeight: COLUMN_FOOTER_HEIGHT
   },
   colorSchemes: {
     light: {},
