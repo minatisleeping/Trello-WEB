6b3479f339f5553f3878d719fa983e01d71a612c
minatisleeping <minatt2002@gmail.com>
2023-12-13 14:02:23 +0700

    drag drop columns

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,19 +1,80 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
-import mapOrder from '~/utils/sorts'
+import { mapOrder } from '~/utils/sorts'
+
+import {
+  DndContext,
+  // PointerSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useEffect, useState } from 'react'
 
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.memberIds, '_id')
+
+  // Nếu dùng PointerSensor mặc  định thì phải kết hợp thuộc tính CSS touch-action: none  ở những phần tử kéo thả
+  //nhưng mà còn byg
+  // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+
+  // Yêu cầu chuột di chuyển 10px thì mới kích hoạt event, fix trường hợp click bị gọi event
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+
+  // Nhấn giữ 250ms và dung sai của cảm ứng(dễ hiểu là di chuyển/chênh lệch 500px) thì mới kích hoạt event
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+
+  // Ưu tiên sử dụng kết hợp 2 loại sensors là mouse và touch
+  //để có trải nghiệm trên mobile tốt nhất, không bị bug
+  // const sensors = useSensors(pointerSensor)
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    // console.log('handleDragEnd: ', event)
+    const { active, over } = event
+
+    // Kiểm tra nếu không tổn tại over (kéo linh tinh ra ngoài thì return luôn tránh lỗi)
+    if (!over) return
+
+    // Nếu vị trí sau khi kéo thả khác với vị trí ban đầu
+    if (active.id !== over.id) {
+      //Lấy vị trí cũ(từ thằng active)
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      //Lấy vị trí mới(từ thằng over)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+
+      // Dùng arrayMove của thằng dnd-kit để sắp xếp lại mảng Columns ban đầu
+      // Code của arrayMove ở đây: dnd-kit/packages/sortable/utilities/arrayMove.ts
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // 2 cái console.log() dữ liệu này sau dùng để xử lý gọi API
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      // console.log('dndorderedColumns: ', dndOrderedColumns)
+      // console.log('dndOrderedColumnsIds: ', dndOrderedColumnsIds)
+
+      // Cập nhật lại state columns ban đầu sau khi đã kéo thả
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
+
   return (
-    <Box sx={{
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
-      <ListColumns columns={orderedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box sx={{
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
